fcbcd7885e6460e6854c118d798153c55b445099
Deb <deb@rasello.com>
2016-12-07 17:07:39 +0545

    changes

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\Redis;
+class TestController extends Controller
+{
+    //
+    
+    // start of index
+    public function index()
+    {
+
+
+        return view('welcome');
+    
+    }// end of index
+}
